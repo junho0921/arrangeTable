b5b89df4cd0c6e5f3bb19be4e09c396050b3ce0b
jun.ho <188630703@qq.com>
2016-05-11 10:49:06 +0800

    文档

--- DraggableMenu.js
+++ DraggableMenu.js
@@ -6,7 +6,7 @@
 (function(factory) {
 	'use strict';
 	if (typeof define === 'function' && define.amd) {
-		define(['jquery'], factory);
+		define(['jquery', 'cssProp'], factory);
 	} else if (typeof exports !== 'undefined') {
 		module.exports = factory(require('jquery'));
 	} else {
